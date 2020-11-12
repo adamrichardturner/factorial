47d69715b0903aa0f23d373bba9f4b0243c58da8
adamrichardturner <hello@adamrichardturner.dev>
2020-11-12 20:35:44 +0000

    Added an error message to the implementation code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Calculate = {
         if (num === 0) {
             return 1;
         } else if (num < 0) {
-            return -1;
+            throw new Error('-1. We are only calculating n! of positive integers.');
         } else {
             return num * this.factorial(num - 1);
         }
